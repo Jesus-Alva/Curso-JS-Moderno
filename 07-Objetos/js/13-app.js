2c8833718ff8a44e95deb24085f6c9e1aa2e2dfd
Jesus-Alva <57468191+Jesus-Alva@users.noreply.github.com>
2024-01-22 21:30:22 -0600

    Curso JS - Object Functions

---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,11 @@
+
+
+const producto = {
+    nombre: 'Monitor de 20 pulgadas',
+    precio: 1200,
+    disponible: true,
+}
+
+console.log(Object.keys(producto));
+console.log(Object.values(producto));
+console.log(Object.entries(producto))
